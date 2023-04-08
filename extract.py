8f4858328788fd4e4e8c4884f4e18bcd0e7a1f06
FengchunFan <ffan005@ucr.edu>
2023-04-08 12:37:36 -0700

    fixed bug and load VGG16 model

--- extract.py
+++ extract.py
@@ -1,3 +1,11 @@
+'''
+import the relevant Keras libraries
+'''
+import os
+import keras
+from keras.preprocessing import image
+from keras.applications.imagenet_utils import decode_predictions, preprocess_input
+from keras.models import Model
 
-
-print("test")
+model = keras.applications.VGG16(weights='imagenet', include_top=True)
+model.summary()
