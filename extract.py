dd0d020470083146028fc12acab63956560c72c5
FengchunFan <ffan005@ucr.edu>
2023-04-08 12:39:29 -0700

    change syntax for easier read

--- extract.py
+++ extract.py
@@ -1,11 +1,10 @@
-'''
-import the relevant Keras libraries
-'''
+#import the relevant Keras libraries
 import os
 import keras
 from keras.preprocessing import image
 from keras.applications.imagenet_utils import decode_predictions, preprocess_input
 from keras.models import Model
 
+#load pretraind cnn model VGG16 from keras
 model = keras.applications.VGG16(weights='imagenet', include_top=True)
 model.summary()
