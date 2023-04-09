17746653a76dcb912ec275fac3b9621029d48612
FengchunFan <ffan005@ucr.edu>
2023-04-08 20:30:37 -0700

    modify the code to support multiply image classification

--- extract.py
+++ extract.py
@@ -17,33 +17,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # load_image function taken from https://colab.research.google.com/github/ml4a/ml4a/blob/master/examples/info_retrieval/image_search.ipynb
+# load_images function modified based on the above function to take multiple input images
 # load image function will preprocess image into correct size of 224x224x3, which is done through load_img function
 # expand_dims is needed to add one more dimension in the very front, to process multiple image input
-def load_image(path):
-    img = image.load_img(path, target_size=model.input_shape[1:3])
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0) 
-    x = preprocess_input(x)
-    return img, x
+def load_images(paths):
+    images = []
+    for path in paths:
+        img = image.load_img(path, target_size=model.input_shape[1:3])
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+        images.append((img, x))
+    return images
 
 from tensorflow.keras.preprocessing import image
 
-# load an image to test the functionality of load_image function
-# output img is the image loaded from input path
-# x is the Numpy array representation of the input image, it has shape of (1, height, width, channels)4
-img, x = load_image("C:/study/Github_Project/Feature-Extraction-CNN/Objects/butterfly/image_0001.jpg")
-print("shape of x: ", x.shape)
-# print("data type: ", x.dtype)
-# print("x: ", x)
-# plt.imshow(img)
-# plt.title("sample image from dataset")
-# plt.show()
-
+# load an or multiple images to test the functionality of load_image function
+# output image is in format of (img, x)
+# img or image[0] is the image loaded from input path
+# x or image [1] is the Numpy array representation of the input image, it has shape of (1, height, width, channels)
 # Predict the class of the image through VGG16 model using built in function
 # input will be Numpy array x, which we extracted previously
 # using [0] to output best prediction
 # _ is needed because first argument being returned is label and we don't need that
-predictions = model.predict(x)
-for _, prediction, probability in decode_predictions(predictions)[0]:
-    print("For sample input x, model predicted it to be {} with probability {:.3%}".format(prediction, probability))
+paths = ["C:/study/Github_Project/Feature-Extraction-CNN/Objects/butterfly/image_0001.jpg", "C:/study/Github_Project/Feature-Extraction-CNN/Objects/crayfish/image_0015.jpg"]
+images = load_images(paths)
+for single_image in images:
+    print("shape of image1: ", single_image[1].shape)
+    plt.imshow(single_image[0])
+    plt.show()
+    predictions = model.predict(single_image[1])
+    for _, prediction, probability in decode_predictions(predictions)[0]:
+        print("For the input image, model predicted it to be {} with probability {:.3%}".format(prediction, probability))
 
